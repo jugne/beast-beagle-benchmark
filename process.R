b77b06fc70f3cbbbb2f39ab0b5352182a2b955ee
jugne <ugne.stolz@bsse.ethz.ch>
2024-11-05 10:45:20 +0100

    running and post-processing scripts

---
+++ process.R
@@ -0,0 +1,94 @@
+wd <- "~/Documents/Source/beast2.7/beast-beagle-benchmark/"
+setwd(wd)
+
+# Load the data
+data <- read.csv("data.csv")
+
+# Add a row number column
+data$row_number <- seq_len(nrow(data))
+
+# Group by columns 2, 3, and 4, then create a list of row numbers for each group
+library(dplyr)
+grouped_data <- data %>%
+  group_by(nPartitions, nTaxa, partitionSeqLength) %>%
+  summarise(row_numbers = list(row_number), .groups = 'drop')
+
+
+best_table <- data.frame() # Best running configurations for each data set up
+best_table_cpu <- data.frame() # Best running configurations for each data set up, when using only CPUs
+diff_table <- data.frame() # Worst and best running configurations for each data set up
+diff_table_cpu <- data.frame() # Worst and best running configurations for each data set up, when using only CPUs
+diff_table_cpu_gpu <- data.frame() # Best running configurations for CPU and GPU, for each data set up
+
+
+for (g in 1:nrow(grouped_data)){
+  # Full running results table for a particular data configuration
+  # Data configuration is determined by # partitions, # taxa, sequence length and total site patterns
+  config_table <- as.data.frame(matrix(NA, nrow = length(grouped_data$row_numbers[[g]]),
+                                       ncol = ncol(data)+1))
+  colnames(config_table) <- c(colnames(data[-9]), "time(minutes)", "totalSitePatterns")
+  l = 1
+  for (i in grouped_data$row_numbers[[g]]){
+    file_path <- list.files(pattern = paste0("^",i,"_.*out"), full.names = TRUE)[[1]]
+    # Read the file lines
+    lines <- readLines(file_path)
+    # Find lines with site patterns
+    pattern_lines <- grep("\\b[0-9]+ patterns\\b", lines, value = TRUE)
+    # Extract the numbers from those lines
+    pattern_counts <- as.numeric(sub("([0-9]+) patterns", "\\1", pattern_lines))
+    # Find the line that contains "Total calculation time"
+    time_line <- grep("Total calculation time:", lines, value = TRUE)
+    
+    # Extract the numeric time value from the line
+    if (length(time_line) > 0) {
+      time_minutes <- as.numeric(sub(".*Total calculation time: ([0-9.]+) seconds.*",
+                                     "\\1", time_line)) / 60
+    } else {
+      stop(paste("Time not found for run,",i))
+    }
+    config_table[l, ]<- c(data[i,-9], time_minutes, sum(pattern_counts))
+    l = l + 1
+  }
+  # Reorder columns so that site patterns are grouped with data configuration
+  config_table <- config_table[,c(1:4,10,5:9)]
+  # Save table for all results, given a particular configuration.
+  # Table is named: nTaxa_seqLength_nPartitions_totalSitePatterns
+  write.csv(config_table,paste0(paste(grouped_data[g,2], 
+                                      grouped_data[g,3],
+                                      grouped_data[g,1],
+                                      config_table[1,5],
+                                      sep="_"), ".csv"))
+  best_table <- rbind(best_table,
+                      config_table[which.min(config_table$`time(minutes)`),])
+  diff_table <- rbind(diff_table,
+                      config_table[which.max(config_table$`time(minutes)`),])
+  diff_table <- rbind(diff_table,
+                      config_table[which.min(config_table$`time(minutes)`),])
+  
+  tmp <- config_table[config_table$nGPU==0,]
+  best_table_cpu <- rbind(best_table_cpu,
+                          tmp[which.min(tmp$`time(minutes)`),])
+  diff_table_cpu <- rbind(diff_table_cpu,
+                          tmp[which.max(tmp$`time(minutes)`),])
+  diff_table_cpu <- rbind(diff_table_cpu,
+                          tmp[which.min(tmp$`time(minutes)`),])
+  
+  tmp1 <- config_table[config_table$nGPU>0,]
+  diff_table_cpu_gpu <- rbind(diff_table_cpu_gpu,
+                              tmp[which.min(tmp$`time(minutes)`),])
+  diff_table_cpu_gpu <- rbind(diff_table_cpu_gpu,
+                              tmp1[which.min(tmp1$`time(minutes)`),])
+}
+
+write.csv(best_table,"best.csv")
+write.csv(best_table_cpu,"best_cpu.csv")
+write.csv(diff_table,"best_vs_worst.csv")
+write.csv(diff_table_cpu,"best_vs_worst_cpu.csv")
+write.csv(diff_table_cpu_gpu,"best_cpu_vs_gpu.csv")
+
+
+
+
+
+
+
--- run_xmls.sh
+++ run_xmls.sh
@@ -1,7 +1,6 @@
 source .env
-
 module load stack/2024-06 openjdk/17.0.8.1_1 libbeagle/3.1.2 beast2/2.7.4
-#packagemanager -add BEASTLabs
+#packagemanager -add BEASTLabs # This package is not available with MultiPartitionTreeLikelihood through Beauti yet. Have to build and install manually.
 packagemanager -add remaster
 packagemanager -add feast
 
@@ -13,20 +12,24 @@ echo "run,nPartitions,nTaxa,partitionSeqLength,nCPU,instances,beagle,nGPU" > "$o
 for taxa in "${nTaxaSingleP[@]}"; do
   for length in "${sequenceLengthSingleP[@]}"; do
     for ins in "${instances[@]}"; do
-      inst=""
-      if [ ${ins} = "true" ]; then
-        inst="-instances ${c}"
-      fi
       for c in "${cpus[@]}"; do
+        inst=""
+        if [ ${ins} = "true" ]; then
+          inst="-instances ${c}"
+        fi
         for r in "${beagleCPUCommand[@]}"; do
-          eval "sbatch -J ${n} --time=48:00:00 --output \"${n}_${taxa}_${length}_1_cpu_${c}_${r}_${ins}.out\" --open-mode=truncate --ntasks="${c}" --wrap=\"beast -seed ${seed} -${r} ${inst} -threads ${c} -D \"nTaxa=${taxa},sl=${length},range=1\" ${xmlSingleName}\""
-          echo "n,1,${taxa},${length},${c},${ins},${r},0" >> "$output_file"
+          eval "sbatch -J ${n} --time=48:00:00 --mem-per-cpu=$((4/${c}))G --output \"${n}_${taxa}_${length}_1_cpu_${c}_${r}_${ins}.out\" --open-mode=truncate --ntasks="${c}" --wrap=\"beast -seed ${seed} -${r} ${inst} -threads ${c} -D \"nTaxa=${taxa},sl=${length},range=1\" ${xmlSingleName}\""
+          echo "${n},1,${taxa},${length},${c},${ins},${r},0" >> "$output_file"
           let n+=1
         done
       done
       for g in "${gpus[@]}"; do
-        eval "sbatch -J ${n} --time=48:00:00 --output \"${n}_${taxa}_${length}_1_gpu_${g}_${ins}.out\" --open-mode=truncate --gpus ${g} --wrap=\"beast -seed ${seed} -beagle_GPU -beagle_order $(seq -s "," 1 "$g") ${inst} -threads ${g} -D \"nTaxa=${taxa},sl=${length},range=1\" ${xmlSingleName}\""
-        echo "n,1,${taxa},${length},1,${ins},${r},${g}" >> "$output_file"
+        inst=""
+        if [ ${ins} = "true" ]; then
+          inst="-instances ${g}"
+        fi
+        eval "sbatch -J ${n} --time=48:00:00 --mem-per-cpu=$((4/${c}))G --output \"${n}_${taxa}_${length}_1_gpu_${g}_${ins}.out\" --open-mode=truncate --gpus ${g} --wrap=\"beast -seed ${seed} -beagle_GPU -beagle_order $(seq -s "," 1 "$g") ${inst} -threads ${g} -D \"nTaxa=${taxa},sl=${length},range=1\" ${xmlSingleName}\""
+        echo "${n},1,${taxa},${length},1,${ins},beagle_GPU,${g}" >> "$output_file"
         let n+=1
       done
     done
@@ -38,20 +41,28 @@ for taxa in "${nTaxaMultiP[@]}"; do
   for length in "${sequenceLengthMultiP[@]}"; do
     for p in "${nPartitions[@]}"; do
       for ins in "${instances[@]}"; do
-        inst=""
-        if [ ${ins} = "true" ]; then
-        inst="-instances ${c}"
-        fi
         for c in "${cpus[@]}"; do
+          inst=""
+          if [ ${ins} = "true" ]; then
+            inst="-instances ${c}"
+          fi
           for r in "${beagleCPUCommand[@]}"; do
-            eval "sbatch -J ${n} --time=48:00:00 --output \"${n}_${taxa}_${length}_${p}_cpu_${c}_${r}_${ins}.out\" --open-mode=truncate --ntasks="${c}" --wrap=\"beast -seed ${seed} -${r} ${inst} -threads ${c} -D \"nTaxa=${taxa},sl=${length},range=$(seq -s "," 1 "$p")\" ${xmlMultiName}\""
-            echo "n,${p},${taxa},${length},${c},${ins},${r},0" >> "$output_file"
+            if [ ${r} = "java" ]; then
+              eval "sbatch -J ${n} --time=48:00:00 --mem-per-cpu=$((4/${c}))G --output \"${n}_${taxa}_${length}_${p}_cpu_${c}_${r}_${ins}.out\" --open-mode=truncate --ntasks="${c}" --wrap=\"beast -seed ${seed} -${r} ${inst} -threads ${c} -D \"nTaxa=${taxa},sl=${length},range=$(seq -s "," 1 "$p")\" ${xmlSingleName}\""
+            else
+              eval "sbatch -J ${n} --time=48:00:00 --mem-per-cpu=$((4/${c}))G --output \"${n}_${taxa}_${length}_${p}_cpu_${c}_${r}_${ins}.out\" --open-mode=truncate --ntasks="${c}" --wrap=\"beast -seed ${seed} -${r} ${inst} -threads ${c} -D \"nTaxa=${taxa},sl=${length},range=$(seq -s "," 1 "$p")\" ${xmlMultiName}\""
+            fi
+            echo "${n},${p},${taxa},${length},${c},${ins},${r},0" >> "$output_file"
             let n+=1
           done
         done
         for g in "${gpus[@]}"; do
-            eval "sbatch -J ${n} --time=48:00:00 --output \"${n}_${taxa}_${length}_${p}_gpu_${g}_${ins}.out\" --open-mode=truncate --gpus ${g} --wrap=\"beast -seed ${seed} -beagle_GPU -beagle_order $(seq -s "," 1 "$g") ${inst} -threads ${g} -D \"nTaxa=${taxa},sl=${length},range=$(seq -s "," 1 "$p")\" ${xmlMultiName}\""
-            echo "n,${p},${taxa},${length},1,${ins},${r},${g}" >> "$output_file"
+          inst=""
+          if [ ${ins} = "true" ]; then
+            inst="-instances ${g}"
+          fi          
+            eval "sbatch -J ${n} --time=48:00:00 --mem-per-cpu=$((4/${c}))G --output \"${n}_${taxa}_${length}_${p}_gpu_${g}_${ins}.out\" --open-mode=truncate --gpus ${g} --wrap=\"beast -seed ${seed} -beagle_GPU -beagle_order $(seq -s "," 1 "$g") ${inst} -threads ${g} -D \"nTaxa=${taxa},sl=${length},range=$(seq -s "," 1 "$p")\" ${xmlMultiName}\""
+            echo "${n},${p},${taxa},${length},1,${ins},beagle_GPU,${g}" >> "$output_file"
             let n+=1
         done
       done
